27db631541cd1371f6f168515aa800041659cd43
jgkim <202020506@sonline20.sen.go.kr>
2021-07-14 10:02:46 +0900

    초기화

--- app/src/main/java/com/example/myproject2/MainActivity.java
+++ app/src/main/java/com/example/myproject2/MainActivity.java
@@ -3,12 +3,67 @@ package com.example.myproject2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+    TextView text1 , text2;
+    CheckBox ChkAgree;
+    RadioGroup rGroup1;
+    RadioButton rdoDog,rdoCat,rdoRabbit;
+    Button btnOK;
+    ImageView imgPet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("애완동물 사진보기");
+        text1 = findViewById(R.id.Text1);
+        text2 = findViewById(R.id.Text2);
+        ChkAgree = findViewById(R.id.ChkAgree);
+        rGroup1 = findViewById(R.id.Rgroup1);
+        rdoDog = findViewById(R.id.RdoDog);
+        rdoCat = findViewById(R.id.RdoCat);
+        rdoRabbit = findViewById(R.id.RdoRabbit);
+        btnOK = findViewById(R.id.BtnOk);
+        imgPet = findViewById(R.id.ImgPet);
+
+        ChkAgree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(ChkAgree.isChecked() == true){
+                    text2.setVisibility(View.VISIBLE);
+                    rGroup1.setVisibility(View.VISIBLE);
+                    btnOK.setVisibility(View.VISIBLE);
+                    imgPet.setVisibility(View.VISIBLE);
+                }
+                else{
+                    text2.setVisibility(View.INVISIBLE);
+                    rGroup1.setVisibility(View.INVISIBLE);
+                    btnOK.setVisibility(View.INVISIBLE);
+                    imgPet.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (rGroup1.getCheckedRadioButtonId()) {
+                    case R.id.RdoDog:
+                        imgPet.setImageResource(R.drawable.dog);
+                        break;
+                    case R.id.RdoCat:
+                        imgPet.setImageResource(R.drawable.cat);
+                        break;
+                    case R.id.RdoRabbit:
+                        imgPet.setImageResource(R.drawable.rabbit);
+                        break;
+                    default:
+                        Toast.makeText(getApplicationContext(),"동물 먼저 선택하세요",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
